de0158a4a98c4ef5192f4a5b4a5874e4dc6454ea
Dikshant Jain <dikshantjn@gmail.com>
2025-01-08 11:51:33 +0530

    Header change

--- src/secondary/Header.js
+++ src/secondary/Header.js
@@ -30,14 +30,18 @@ function Header() {
             {/* Main Navigation with Burger Menu */}
             <nav className={`main-nav ${isNavOpen ? 'open' : ''}`}>
                 <div className="nav-links">
-                    <Link to="/products">Products</Link>
-                    <Link to="/lab-tests">Lab Tests</Link>
-                    <Link to="/doctors">Consult Doctors</Link>
-                    <Link to="/blood-bank">Blood Bank</Link>
-                    <Link to="/ambulance">Ambulance</Link>
-                    <Link to="/medical-loans">Medical Loans</Link>
-                    <Link to="/insurance">Medical Insurance</Link>
-                    <Link to="/vaccines">Vaccines</Link>
+                <Link to="/products">PRODUCTS</Link>
+                    <Link to="/lab-tests">LAB TESTS</Link>
+                    <Link to="/doctors">CONSULT DOCTORS</Link>
+                    <Link to="/blood-bank">BLOOD BANK</Link>
+                    <Link to="/ambulance">AMBULANCE</Link>
+                    <Link to="/medical-loans">MEDICAL LOANS</Link>
+                    <Link to="/insurance">MEDICAL INSURANCE</Link>
+                    <Link to="/vaccines">VACCINES</Link>
+                    <Link to="/maternal-care">MATERNAL CARE</Link>
+                    <Link to="/child-care">CHILD CARE</Link>
+                    <Link to="/delivery">MEDICINE DELIVERY</Link>
+                    <Link to="/ayurveda">AYURVEDA</Link>
                 </div>
 
                 {/* Buttons moved to hamburger menu */}
@@ -79,10 +83,16 @@ function Header() {
                 </div>
 
                 <div className="sub-nav-links">
+                    <Link to="/physiotherapy">Physiotherapy</Link>
+                    <Link to="/hosiptal-discovery">Hospital Discovery</Link>
+                    <Link to="/care-at-home">Care At Home</Link>
+                    <Link to="/medical-tourism">Medical Tourism</Link>
                     <Link to="/nutrition">Nutrition</Link>
                     <Link to="/rehabilitation">Rehabilitation</Link>
                     <Link to="/early-detection">Early Detection</Link>
+                    <Link to="/nutrition">Nutrition</Link>
                     <Link to="/pet-care">Pet Care</Link>
+                    <Link to="/organ-donation">Organ/Hair Donation</Link>
                 </div>
             </div>
         </header>
